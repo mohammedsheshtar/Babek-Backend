a6d651c3f9be8ce652b445d709bfe0bdfff7eecb
mohammedsheshtar <112146380+mohammedsheshtar@users.noreply.github.com>
2025-07-09 07:57:10 +0300

    Initialized Task

---
+++ src/main/kotlin/com/nbk/babek/BabekApplication.kt
@@ -0,0 +1,11 @@
+package com.nbk.babek
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class BabekApplication
+
+fun main(args: Array<String>) {
+	runApplication<BabekApplication>(*args)
+}
---
+++ src/test/kotlin/com/nbk/babek/BabekApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.nbk.babek
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class BabekApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
